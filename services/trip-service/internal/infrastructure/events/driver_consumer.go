c59d8c9730f360c05e762a691fde6132d7e2808e
psttrmtc <minhloi12300@gmail.com>
2025-08-29 20:30:42 +0700

    feat: trip decline flow

--- services/trip-service/internal/infrastructure/events/driver_consumer.go
+++ services/trip-service/internal/infrastructure/events/driver_consumer.go
@@ -46,13 +46,40 @@ func (c *driverConsumer) Listen() error {
 				return err
 			}
 		case contracts.DriverCmdTripDecline:
-			log.Println("Declined")
+			if err := c.handleTripDeclined(ctx, payload.TripID, payload.RiderID); err != nil {
+				log.Printf("Failed to handle the trip declined: %v", err)
+			}
 			return nil
 		}
 		log.Printf("Unknow trip event: %+v", payload)
 		return nil
 	})
 }
+
+func (c *driverConsumer) handleTripDeclined(ctx context.Context, tripID, riderID string) error {
+	// When a driver declines, we should try to find another driver
+	trip, err := c.service.GetTripByID(ctx, tripID)
+	if err != nil {
+		return err
+	}
+
+	newPayload := messaging.TripEventData{
+		Trip: trip.ToProto(),
+	}
+
+	marshalledPayload, err := json.Marshal(newPayload)
+	if err != nil {
+		return err
+	}
+	if err := c.rabbitmq.PublishMessage(ctx, contracts.TripEventDriverNotInterested, contracts.AmqpMessage{
+		OwnerID: riderID,
+		Data:    marshalledPayload,
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (c *driverConsumer) handleTripAccepted(ctx context.Context, tripID string, driver *pbd.Driver) error {
 	// 1. Fetch the first
 	trip, err := c.service.GetTripByID(ctx, tripID)
@@ -60,7 +87,7 @@ func (c *driverConsumer) handleTripAccepted(ctx context.Context, tripID string, 
 		return err
 	}
 	if trip == nil {
-		return fmt.Errorf("Trip was not found %s", tripID)
+		return fmt.Errorf("trip was not found %s", tripID)
 	}
 
 	// 2. Update the trip
